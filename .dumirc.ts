5e50f4554cd2ff1650d47b84b95b0553735cf387
cyuinhsin <chenyunxin6@gmail.com>
2025-02-04 07:54:32 +0800

    feat: 初步运行成功

--- .dumirc.ts
+++ .dumirc.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'dumi';
 
 export default defineConfig({
   outputPath: 'docs-dist',
+  base: '/notes/',
+  publicPath: '/notes/',
   themeConfig: {
     name: 'notes',
   },
